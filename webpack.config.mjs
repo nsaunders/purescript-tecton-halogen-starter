e4401b602217704b268bce5c0b8f29edcf1140bd
Nick Saunders <nick@saunde.rs>
2023-01-28 11:43:43 -0700

    Upgrade execute-module-loader to v1.0.0.

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -46,12 +46,7 @@ export default ({ production }) => ({
           MiniCssExtractPlugin.loader,
           "css-loader",
           "postcss-loader",
-          {
-            loader: "execute-module-loader",
-            options: {
-              getResult: x => x.sheet,
-            },
-          },
+          "execute-module-loader?export=sheet",
         ],
       },
       {
